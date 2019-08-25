89ae40aa1dad0bce8433e80b12229c8aec38df54
ttran9 <toddtran9@gmail.com>
2019-08-24 22:05:20 -0700

    creating component to display the registration form and to process the user registration.

---
+++ src/components/auth/Registration.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class Registration extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+          email: "",
+          password: "",
+          password_confirmation: "",
+          registrationErrors: ""
+        };
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        const {
+            email,
+            password,
+            password_confirmation
+        } = this.state;
+        axios.post("http://localhost:3001/registrations", {
+            user: {
+                email: email,
+                password: password,
+                password_confirmation: password_confirmation
+            }
+        },
+        /*
+         * without "withCredentials" we are in an "in between state" where we have created a user or logged them in successfully but the 
+         * cookie inside of the browser will not show that the user is logged in.
+         */
+        { withCredentials: true} // this third argument tells the API to set the cookie in our client.
+        ).then(response => {
+            console.log("registration response", response);
+        }).catch(error => {
+            console.log("registration error", error);
+        });
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div>
+               <form onSubmit={this.handleSubmit}>
+                <input type="email" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} required />
+                <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} required />
+                <input type="password" name="password_confirmation" placeholder="Password Confirmation" value={this.state.password_confirmation} onChange={this.handleChange} required />
+                <button type="submit">Register</button>
+               </form>
+            </div>
+        );
+    }
+};
+
