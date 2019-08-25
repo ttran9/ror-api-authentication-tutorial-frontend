dc6d0d6100d76910522c3057b836b791674b2f4b
ttran9 <toddtran9@gmail.com>
2019-08-25 13:14:47 -0700

    displaying the login form.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Registration from './auth/Registration';
+import Login from './auth/Login';
 
 export class Home extends Component {
     constructor(props) {
@@ -17,7 +18,10 @@ export class Home extends Component {
             <div>
              <h1>Home</h1>   
              <h1>Status: {this.props.loggedInStatus} </h1>   
+             <br/>
              <Registration handleSuccessfulAuth={this.handleSuccessfulAuth}/>
+             <br/>
+             <Login handleSuccessfulAuth={this.handleSuccessfulAuth} />
             </div>
         )
     }
