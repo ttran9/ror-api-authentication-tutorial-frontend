460a1ab0fd99826d6769c7b18566f4f971db10e7
ttran9 <toddtran9@gmail.com>
2019-08-24 23:46:37 -0700

    displaying the updated status and passing in the handleSuccessfulAuth to the child Registration component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react'
 import Registration from './auth/Registration';
 
 export class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleSuccessfulAuth = data => {
+        // update parent component.
+        this.props.handleLogin(data);
+        this.props.history.push("/dashboard");
+    }
+
     render() {
         return (
             <div>
              <h1>Home</h1>   
-             <h1>Home</h1>   
-             <Registration/>
+             <h1>Status: {this.props.loggedInStatus} </h1>   
+             <Registration handleSuccessfulAuth={this.handleSuccessfulAuth}/>
             </div>
         )
     }
